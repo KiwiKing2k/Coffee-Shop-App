45dc11f556aed0ca320bea5c37ce511682194e32
Fireballer <dinustefan04@gmail.com>
2025-01-08 00:17:46 +0200

    Testing for observer creation

--- main.cpp
+++ main.cpp
@@ -15,20 +15,25 @@ int main()
     DebaraDirector debara_director;
 
     debara_director.set_builder(&romanian_debara_builder);
-    Debara romanian_debara = debara_director.construct_debara();
+    Debara romanian_debara;
+    romanian_debara= debara_director.construct_debara();
+    ConcreteObserver ro_observer("Observer1");
+    romanian_debara.register_observer(&ro_observer);
 
-    for (const auto& ingredient : romanian_debara.ingrediente)
+    /*for (const auto& ingredient : romanian_debara.ingrediente)
     {
         cout << ingredient.nume << " " << ingredient.cantitate << " " << ingredient.cost << endl;
-    }
+    }*/
 
-    debara_director.set_builder(&english_debara_builder);
+    /*debara_director.set_builder(&english_debara_builder);
     Debara english_debara = debara_director.construct_debara();
 
     for (const auto& ingredient : english_debara.ingrediente)
     {
         cout << ingredient.nume << " " << ingredient.cantitate << " " << ingredient.cost << endl;
-    }
+    }*/
+
+
 
     return 0;
 }
